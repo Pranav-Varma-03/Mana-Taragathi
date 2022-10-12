e96a396706af085fd60f8b54f407870de54612cc
Pranav-Varma-03 <harshapranav323@gmail.com>
2022-10-12 14:59:40 +0530

    Reset Password added

---
+++ app/src/main/java/com/example/taragati06/changePassword.kt
@@ -0,0 +1,26 @@
+package com.example.taragati06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class changePassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+
+    fun SaveChangesPwd(view : View){
+
+
+        //take input of old password
+        //take input of new password
+        //take input of confirm password
+
+        //check and examine, and confirm changes
+
+        // return to Login Page
+
+
+    }
+}
